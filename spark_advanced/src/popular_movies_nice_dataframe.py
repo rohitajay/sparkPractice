d934d2c809d8c5967ca86abb909811139cb79e12
Rohit <rohit.gupta@cognologix.com>
2022-09-26 20:37:58 +0530

    Initial commit

---
+++ spark_advanced/src/popular_movies_nice_dataframe.py
@@ -0,0 +1,54 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, LongType
+import codecs
+
+def loadMovieNames():
+    movieNames = {}
+
+    with codecs.open("/home/administrator/PycharmProjects/sparkProject/spark_advanced/Data/ml-100k/u.item", "r", encoding="ISO-8859-1", errors= 'ignore') as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+
+spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
+
+nameDict = spark.sparkContext.broadcast(loadMovieNames())
+
+# Create Schema when reading u.data
+
+schema = StructType([\
+    StructField("userID", IntegerType(), True),
+    StructField("movieID", IntegerType(), True),
+    StructField("rating", IntegerType(), True),
+    StructField("timestamp", LongType(), True)])
+
+
+#Load up movie data as dataframe
+
+moviesDF = spark.read.option("sep", "\t").schema(schema=schema).csv("/home/administrator/PycharmProjects/sparkProject/spark_advanced/Data/ml-100k/u.data")
+
+movieCounts = moviesDF.groupBy("movieID").count()
+# print(movieCount)
+
+# Create a user-defined function to look up movie names from our broadcasted dictionary
+
+def lookupName(movieID):
+    return nameDict.value[movieID]
+
+lookupNameUDF = func.udf(lookupName)
+
+# Add a movieTitle column using our new udf
+
+moviesWithNames = movieCounts.withColumn("movieTitle", lookupNameUDF(func.col("movieID")))
+
+# Sort the results
+sortedMoviesWithNames = moviesWithNames.orderBy(func.desc("count"))
+
+# Sort the results
+sortedMoviesWithNames.show(10, False)
+
+spark.stop()
+
